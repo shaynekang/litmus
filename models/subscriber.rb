234269ab07af5c2d74abf6320b85123a797dd29d
paranoiase Kang <paranoiase@gmail.com>
2012-03-22 21:05:54 +0900

    localizing

--- models/subscriber.rb
+++ models/subscriber.rb
@@ -1,12 +1,14 @@
+# encoding: UTF-8
+
 class Subscriber
   include DataMapper::Resource
-  
+
   property :id, Serial, unique: true
   property :email, String, :required => true, :unique => true,
      :format   => :email_address,
      :messages => {
-       :presence  => 'We need your email address.',
-       :is_unique => 'We already have that email.',
-       :format    => "Doesn't look like an email address to me ..."
+       :presence  => '이메일 주소를 입력해주세요.',
+       :is_unique => '같은 이메일이 이미 등록되어 있습니다.',
+       :format    => "올바른 이메일 주소를 입력해주세요."
      }
 end
