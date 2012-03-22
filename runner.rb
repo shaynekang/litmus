87ab134b6b466480a229aae0c3aecfa6dff9b1c9
paranoiase Kang <paranoiase@gmail.com>
2012-03-22 22:19:23 +0900

    refactoring Subscribers count

--- runner.rb
+++ runner.rb
@@ -4,7 +4,6 @@ require 'sinatra'
 require './config/bootstrap'
 
 get '/' do
-  @subscribers = Subscriber.all
   @subscriber = Subscriber.new
   slim :index
 end
@@ -15,7 +14,6 @@ post '/subscribe' do
     flash[:notice] = "이메일이 등록되었습니다!"
     redirect to('/')
   else
-    @subscribers = Subscriber.all
     slim :index
   end
 end
